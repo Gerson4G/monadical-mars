632d111769624ee73fa92d4eee3dcc38356d7e02
Gerson Goatache <gersongoatache@gmail.com>
2020-05-31 18:23:03 -0400

    styled structure

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './components';
+
+function Dashboard() {
+  return (
+    <Styled.Container className="App">
+
+    </Styled.Container>
+  );
+}
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
