97f0ca8509b088677249fcba6498116677cf0ac9
Gerson Goatache <gersongoatache@gmail.com>
2020-05-31 19:54:04 -0400

    fix menu open-close with right icon

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -2,41 +2,51 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuIcon from '@material-ui/icons/Menu';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 
 export default function MenuBar() {
-
     const [anchorEl, setAnchorEl] = React.useState(null);
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
 
     return (
         <AppBar position="static">
             <Toolbar>
-                
-                <IconButton
-                    edge="start"
-                    color="inherit" 
-                    aria-label="menu" 
-                    aria-controls="menu-bar" 
-                    aria-haspopup="true" 
-                    onClick={(e) => setAnchorEl(e.currentTarget)}
-                >
-                <Menu
-                    id="menu-bar"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={() => setAnchorEl(null)}
-                >
-                    <MenuItem>Profile</MenuItem>
-                    <MenuItem>My account</MenuItem>
-                    <MenuItem>Logout</MenuItem>
-                </Menu>
-                    <MenuIcon />
-                </IconButton>
+            <MenuIcon
+                edge="start"
+                color="inherit" 
+                aria-label="menu" 
+                aria-controls={"menu-bar"}
+                aria-haspopup="true" 
+                onClick={(e) => {setAnchorEl(e.currentTarget)}}
+            />
+            
+            <Menu
+                id="menu-bar"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                getContentAnchorEl={null}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+             
                 <Typography variant="h6">
                 News
                 </Typography>
