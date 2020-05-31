4a5236224ac5ac33edf0b5e9811ed7aa825d3c02
Gerson Goatache <gersongoatache@gmail.com>
2020-05-31 19:24:57 -0400

    Menu on the menubar

--- src/components/MenuBar/MenuBar.jsx
+++ src/components/MenuBar/MenuBar.jsx
@@ -5,20 +5,43 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-export default function ButtonAppBar() {
+export default function MenuBar() {
 
-  return (
-    <AppBar position="static">
-        <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-            <MenuIcon />
-            </IconButton>
-            <Typography variant="h6">
-            News
-            </Typography>
-            <AccountCircle />
-        </Toolbar>
-    </AppBar>
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                
+                <IconButton
+                    edge="start"
+                    color="inherit" 
+                    aria-label="menu" 
+                    aria-controls="menu-bar" 
+                    aria-haspopup="true" 
+                    onClick={(e) => setAnchorEl(e.currentTarget)}
+                >
+                <Menu
+                    id="menu-bar"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={() => setAnchorEl(null)}
+                >
+                    <MenuItem>Profile</MenuItem>
+                    <MenuItem>My account</MenuItem>
+                    <MenuItem>Logout</MenuItem>
+                </Menu>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6">
+                News
+                </Typography>
+                <AccountCircle />
+            </Toolbar>
+        </AppBar>
   );
 }
