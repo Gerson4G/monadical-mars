6428065b29c7f4a6eae4a41ede8e9fa6d6ded52c
Gerson Goatache <gersongoatache@gmail.com>
2020-05-31 18:38:33 -0400

    dashboard distribution

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,7 +4,11 @@ import * as Styled from './components';
 function Dashboard() {
   return (
     <Styled.Container className="App">
-
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </Styled.Container>
   );
 }
--- src/components/Dashboard/components.js
+++ src/components/Dashboard/components.js
@@ -1,6 +1,38 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+
+    div {
+        border: 2px solid red;
+    }
+
+    div:first-child {
+        grid-column: 1 / -1 ;
+        grid-row: 1;
+    }
+
+    div:nth-child(2) {
+        grid-column: 1;
+        grid-row: 2;
+    }
+
+    div:nth-child(3) {
+        grid-column: 2;
+        grid-row: 2;
+    }
+
+    div:nth-child(4) {
+        grid-column: 1 ;
+        grid-row: 3;
+    }
+
+    div:nth-child(5) {
+        grid-column: 2 ;
+        grid-row: 3;
+    }
 
 `;
 
