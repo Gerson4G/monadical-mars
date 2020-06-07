982f07c566ecd482227ed99ab5f1295abbd36906
Gerson Goatache <gersongoatache@gmail.com>
2020-06-07 10:37:36 -0400

    start video feed comp

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import * as Styled from './components';
 import Chart from '../Chart/Chart';
 import Slideshow from '../Slideshow/Slideshow';
+import VideoFeed from '../VideoFeed/VideoFeed';
 
 function Dashboard() {
   return (
     <Styled.Container className="App">
       <Chart />
       <Slideshow />
-      <div></div>
+      <VideoFeed />
       <div></div>
       <div></div>
     </Styled.Container>
---
+++ src/components/VideoFeed/VideoFeed.jsx
@@ -0,0 +1,13 @@
+
+import React, {useState, useEffect} from 'react';
+
+const VideoFeed = (props) => {
+
+    return (
+        <div>
+            JEJEJE
+        </div>
+     );
+}
+
+export default VideoFeed;
