3e40f1c0e4dd446ee80a7748872f3553db237f7c
Gerson Goatache <gersongoatache@gmail.com>
2020-06-07 12:50:59 -0400

    auto play video and fix src

--- src/components/VideoFeed/VideoFeed.jsx
+++ src/components/VideoFeed/VideoFeed.jsx
@@ -1,11 +1,19 @@
 
 import React, {useState, useEffect} from 'react';
+import { arrayBufferToBase64 } from '../../utils';
 
 const VideoFeed = (props) => {
 
     return (
         <div>
-            JEJEJE
+            <iframe 
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/1Ll-VHYxWXU?autoplay=1&start=2"
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
         </div>
      );
 }
