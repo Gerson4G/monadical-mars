bec2199ac5c35ef599a2615dd52eb0f433599164
Gerson Goatache <gersongoatache@gmail.com>
2020-06-08 20:53:58 -0400

    fix table with react-window

--- src/components/TableData/TableData.jsx
+++ src/components/TableData/TableData.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, findDOMNode } from 'react';
 import TableCell from '@material-ui/core/TableCell';
+import { FixedSizeList as List } from 'react-window';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableContainer from '@material-ui/core/TableContainer';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import RootRef from '@material-ui/core/RootRef';
 import { TableBodyContainer, StyledTable } from './components';
 import { tsvOrCsvToJSON } from '../../utils';
 
@@ -18,12 +20,13 @@ const TableData = (props) => {
   const getData = () => {
     fetch('./data/viking_lander_data.csv', {mode: 'no-cors'})
     .then(response => response.text())
-    .then(data=> setRows(tsvOrCsvToJSON(data, 'csv').map(row => ({...row, id: Math.random()}))))
+    .then(data=> setRows(tsvOrCsvToJSON(data, 'csv')))
   }
 
   const [sort, changeSort] = useState(0);
   const [sortBy, changeSortBy] = useState('');
   const [rows, setRows] = useState(null);
+  const [containerNode, setContainerNode] = useState(null);
 
   useEffect(() => {
     if(!rows){
@@ -48,35 +51,44 @@ const TableData = (props) => {
   const SortableHeader = ({name, label, align = 'right'}) => (
     <TableCell onClick={() => sortTable(name)} align={align}>{label}</TableCell>
   )
-  
+
+  const renderRow = ({style, index}) => {
+  return <TableRow key={index} style={style}>
+    <TableCell align="right">{index+1}</TableCell>
+    {
+      Object.keys(rows[index]).map( property =>
+        <TableCell align="right" key={index}>{rows[index][property]}</TableCell>
+      )
+    }
+  </TableRow>
+} 
+
   if(!rows){
     return <CircularProgress />
   }
 
   return (
-    <TableContainer>
+    <TableContainer ref={setContainerNode}>
       <StyledTable aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell align='center'>Row</TableCell>
             {
               Object.keys(rows[0]).map( header =>
-                <SortableHeader name={header} label={header} />
+                <SortableHeader key={header} name={header} label={header} />
               )
             }
           </TableRow>
         </TableHead>
         <TableBodyContainer>
-          {rows.map( (row, index) => (
-            <TableRow key={row.id}>
-              <TableCell align="right">{index+1}</TableCell>
-              {
-                Object.keys(row).map( property =>
-                  <TableCell align="right">{row[property]}</TableCell>
-                )
-              }
-            </TableRow>
-          ))}
+          <List
+            height={350}
+            itemCount={rows.length ?? 0}
+            itemSize={35}
+            width={containerNode?.clientWidth ?? 0}
+          >
+            {renderRow}
+          </List>
         </TableBodyContainer>
       </StyledTable>
     </TableContainer>
@@ -84,3 +96,4 @@ const TableData = (props) => {
 }
 
 export default TableData;
+
