bc413347ace8bd8dc3e74bfc31e8b0a8b58dac56
Gerson Goatache <gersongoatache@gmail.com>
2020-06-11 21:50:29 -0400

    again add source

--- src/components/VideoFeed/VideoFeed.jsx
+++ src/components/VideoFeed/VideoFeed.jsx
@@ -7,7 +7,7 @@ const VideoFeed = (props) => {
     return (
         <div>
             <video autoPlay loop playsInline width="100%">
-              //<source src="https://mars.nasa.gov/system/video_items/5761_JPL-20190502-MSLf-0001-1920.mp4" type="video/mp4" />
+              <source src="https://mars.nasa.gov/system/video_items/5761_JPL-20190502-MSLf-0001-1920.mp4" type="video/mp4" />
               <p>Your browser doesn't support HTML5 video</p>
             </video>
         </div>
