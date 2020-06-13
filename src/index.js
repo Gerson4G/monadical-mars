594986445d7f63b2de5b4535820f0c219397ed30
Gerson Goatache <gersongoatache@gmail.com>
2020-06-13 19:06:50 -0400

    component to manage languages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import MenuBar from './components/MenuBar/MenuBar';
 import * as serviceWorker from './serviceWorker';
+import {IntlProvider} from "react-intl";
+
+import locale_en from './translations/en.json';
+import locale_es from './translations/es.json';
+
+const data = {
+  'en': locale_en,
+  'es': locale_es,
+};
+
+const App = (props) => {
+
+	const [language, setLanguage] = useState('en');
+
+	return(
+  	<IntlProvider locale={language} messages={data[language]}>
+		<MenuBar setLanguage={setLanguage}/>
+	    <Dashboard />
+	</IntlProvider>
+	);
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <MenuBar />
-    <Dashboard />
+	  <App />  
   </React.StrictMode>,
   document.getElementById('root')
 );
