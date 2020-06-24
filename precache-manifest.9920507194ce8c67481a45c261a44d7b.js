48bb3aa881a7654d40c684379c1e3abec80b66df
Gerson Goatache <gersongoatache@gmail.com>
2020-06-24 19:18:31 -0400

    Updates

---
+++ precache-manifest.9920507194ce8c67481a45c261a44d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1cf33e77b82b06360d35a17ccff07e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a7d309aaf97e0967a49",
+    "url": "./static/js/2.0cdd0a90.chunk.js"
+  },
+  {
+    "revision": "c7a7e79b10ab645cab64be742e1dd9e5",
+    "url": "./static/js/2.0cdd0a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ccb812ec2bfab6dc44",
+    "url": "./static/js/main.5167c544.chunk.js"
+  },
+  {
+    "revision": "53c8a8eede50ce7de3ad",
+    "url": "./static/js/runtime-main.8f3ede06.js"
+  }
+]);
