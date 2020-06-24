7142d81a298fb411d2bfdd7697311c7659bcc6b7
Gerson Goatache <gersongoatache@gmail.com>
2020-06-24 18:48:49 -0400

    Updates

---
+++ precache-manifest.0e96bfc7e7d1b2266cb899a7425b5e53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba250457089c25f2b3905d58b6d020cd",
+    "url": "/monadical-mars/index.html"
+  },
+  {
+    "revision": "5bed444a051dfe0c3906",
+    "url": "/monadical-mars/static/js/2.0d3711e6.chunk.js"
+  },
+  {
+    "revision": "c7a7e79b10ab645cab64be742e1dd9e5",
+    "url": "/monadical-mars/static/js/2.0d3711e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a2c7f233c8765e3450",
+    "url": "/monadical-mars/static/js/main.c6e29c85.chunk.js"
+  },
+  {
+    "revision": "1c93596869cc809e41ec",
+    "url": "/monadical-mars/static/js/runtime-main.69c11bfc.js"
+  }
+]);
